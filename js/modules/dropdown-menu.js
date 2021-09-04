9ecb6e90a7c0788295f90e9742e910a414d993e7
Camila Queiroz <camimq85@gmail.com>
2021-09-04 18:42:18 -0300

    Finaliza o dropdown-menu.

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,15 +1,20 @@
-export default function initDropDownMenu(){}
+import outsideClick from './outsideclick.js';
 
+export default function initDropDownMenu(){
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach( menu => {
-  ['touchstart', 'click'].forEach(userEvent => {
-    menu.addEventListener(userEvent, handleClick);
+  dropdownMenus.forEach( menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    })
   })
-})
 
-function handleClick(event){
-  event.preventDefault();
-  this.classList.toggle('active');
+  function handleClick(event){
+    event.preventDefault();
+    this.classList.add('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    })
+  }
+
 }
